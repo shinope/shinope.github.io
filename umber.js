6417c9a9e762fbd34d159b4953ba33f917c14d41
shinope <112838372+shinope@users.noreply.github.com>
2023-06-29 20:26:27 +0100

    fixes

--- umber.js
+++ umber.js
@@ -1,5 +1,9 @@
-var fname = document.getElementById('#fname');
-var lname = document.getElementById('#lname');
-var email = document.getElementById('#email');
+//var fname = document.getElementById('#fname');
+//var lname = document.getElementById('#lname');
+//var email = document.getElementById('#email');
 
-console.log(fname);
+if (typeof window === "object") {
+    // code is running in a browser environment
+  } else {
+    // code is running in a non-browser environment
+  }
