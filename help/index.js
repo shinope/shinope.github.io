6df1a798b23f35c2b0db5c66c91568a3e6561c22
shinope <112838372+shinope@users.noreply.github.com>
2023-07-02 06:56:21 +0100

    new

---
+++ help/index.js
@@ -0,0 +1,18 @@
+
+// let lap=0;
+
+// function log(){
+//     console.log(lap= lap+ 1);
+// }
+// log();
+// log();
+// log();
+
+let count=0;
+function counts(){
+    console.log("click");
+
+
+}
+
+counts();
--- umber.js
+++
@@ -1,9 +0,0 @@
-//var fname = document.getElementById('#fname');
-//var lname = document.getElementById('#lname');
-//var email = document.getElementById('#email');
-
-if (typeof window === "object") {
-    // code is running in a browser environment
-  } else {
-    // code is running in a non-browser environment
-  }
